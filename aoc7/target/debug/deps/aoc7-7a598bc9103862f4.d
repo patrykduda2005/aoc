b572761fbc5d12137edd4ed91c48f7f0a44481e8
patrykduda2005 <patrykduda7@gmail.com>
2023-12-11 10:22:17 +0100

    udany test

---
+++ aoc7/target/debug/deps/aoc7-7a598bc9103862f4.d
@@ -0,0 +1,5 @@
+/home/duda/Code/rust/aoc/aoc7/target/debug/deps/aoc7-7a598bc9103862f4.rmeta: src/main.rs
+
+/home/duda/Code/rust/aoc/aoc7/target/debug/deps/aoc7-7a598bc9103862f4.d: src/main.rs
+
+src/main.rs:
---
+++ aoc7/target/debug/deps/aoc7-f7e49ae32b57e19a.d
@@ -0,0 +1,5 @@
+/home/duda/Code/rust/aoc/aoc7/target/debug/deps/aoc7-f7e49ae32b57e19a.rmeta: src/main.rs
+
+/home/duda/Code/rust/aoc/aoc7/target/debug/deps/aoc7-f7e49ae32b57e19a.d: src/main.rs
+
+src/main.rs:
